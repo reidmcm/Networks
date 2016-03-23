d03a4ec19f61a46b1e0c8b5c468c63b5a5106ec1
Reid <reidmcm@gmail.com>
2016-03-23 13:25:47 -0400

    First commit

---
+++ commitTest.r
@@ -0,0 +1,14 @@
+
+
+
+
+##Load modules 
+library(Biobase)
+library("GEOquery")
+library("Biobase")
+library("prodlim")
+library("limma")
+library(NMF)
+library(Heatplus)
+library(heatmap3)
+library(GSVA)
